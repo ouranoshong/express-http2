ae31026ed916b12b728a9e0e2b59957abbba0ad7
hong <hong@xiyanghui.com>
2017-05-08 16:53:08 +0800

    Init project

---
+++ index.js
@@ -0,0 +1,27 @@
+const port = 3000
+const spdy = require('spdy')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+app.get('*', (req, res)=> {
+    res.status(200).json({message: 'ok'})
+})
+
+const options = {
+    key: fs.readFileSync(__dirname + '/certs/certificate.key'),
+    cert: fs.readFileSync(__dirname + '/certs/certificate.crt')
+}
+
+spdy
+  .createServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      console.error(error)
+      return process.exit(1)
+    } else {
+      console.log('Listening on port: ' + port + '.')
+    }
+  })
